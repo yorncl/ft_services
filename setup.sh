2493330b6747e17735116f00f0a2c1996392e3f7
Martin CLAUDEL <mclaudel@e2r4p14.42.fr>
2020-02-23 16:35:10 +0100

    grfana to influx db ok telegraf to influx ok but telegraf to kube_inventory not ok

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-# minikube start --extra-config=apiserver.service-node-port-range=80-60000 --cpus 3 --memory=3000mb
-# minikube addons enable ingress
+minikube start --extra-config=apiserver.service-node-port-range=80-60000 --cpus 3 --memory=3000mb
+minikube addons enable ingress
+minikube addons enable metrics-server
 
 kubectl delete -k srcs
 
@@ -8,6 +9,8 @@ docker build -t custom-nginx:1 ./srcs/nginx/
 docker build -t custom-wordpress:1 ./srcs/wordpress/
 docker build -t custom-mysql:1 ./srcs/mysql/
 
+
+kubectl create configmap telegraf-config --from-file=srcs/telegraf/telegraf.conf -o yaml --dry-run | kubectl replace -f - || kubectl create configmap telegraf-config --from-file=srcs/telegraf/telegraf.conf
 kubectl apply -k srcs
 
 minikube dashboard
